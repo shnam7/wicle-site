ca1d765c11586935d9c5f8605bbd22bc0f42a323
Robin Soo-hyuk Nam <shnam7@gmail.com>
2018-07-17 02:42:20 +0900

    Add default <p> style to scss/base/reset.scss

--- docs/assets/scripts/demo/parallax.ts
+++ docs/assets/scripts/demo/parallax.ts
@@ -6,6 +6,7 @@ const SURFACE_COUNT   = 5;
 
 $(function () {
   let parallax = new Wicle.Parallax.Container(<any>window, 0.7);
+  // let parallax = new Wicle.Parallax.Container(document.getElementById('l-site-container'), 0.7);
 
   for (let i=1; i<=SURFACE_COUNT; ++i)
     parallax.addSurface(new Wicle.Parallax.Surface(<any>document.getElementById('surface'+i)))
